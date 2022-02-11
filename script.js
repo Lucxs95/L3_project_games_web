ab19f87968af94676f5956119fc0bc8e6f7afbae
Lucas Blanc <lucasb0695@gmail.com>
2022-02-11 16:45:16 +0100

    last commit of 11/02 16:45

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 window.onload = () => {
-<<<<<<< Updated upstream
+
     parseJSON();
 }
 
@@ -37,56 +37,54 @@ const parseCookie = str =>
         acc[decodeURIComponent(v[0].trim())] = decodeURIComponent(v[1].trim());
         return acc;
     }, {});
-=======
-
-    // connexion ou inscription 
-    //comparer les input sinon creer un player dans json
-    // recuperer les infos du joueurs dans son JSON_player puis stocker dans cookies
 
 
-    let requestURL = 'http://127.0.0.1:5500/json/data.json';
-    var request = new XMLHttpRequest();
-    request.open('GET', requestURL);
-    request.responseType = 'json';
-    request.send();
-    request.onload = function() {
-        let player = request.response;
-        console.log(player);
-    }
+// connexion ou inscription 
+//comparer les input sinon creer un player dans json
+// recuperer les infos du joueurs dans son JSON_player puis stocker dans cookies
 
-    document.cookie = "name=";
-    document.cookie = "password=";
-    document.getElementById("connexion").
-    addEventListener("click", () => {
-        let name = document.getElementById("name").value;
-        let password = document.getElementById("password").value;
-        document.cookie = "name=" + name;
-        document.cookie = "password=" + password;
-    });
+
+let requestURL = 'http://127.0.0.1:5500/json/data.json';
+var request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+request.onload = function() {
+    let player = request.response;
+    console.log(player);
+}
+
+document.cookie = "name=";
+document.cookie = "password=";
+document.getElementById("connexion").
+addEventListener("click", () => {
+    let name = document.getElementById("name").value;
+    let password = document.getElementById("password").value;
+    document.cookie = "name=" + name;
+    document.cookie = "password=" + password;
+});
+document.getElementById("retourPrincipale").style.display = "none";
+
+document.getElementById("inscriptionPrincipale").addEventListener("click", () => {
+    document.getElementById("troisieme").style.display = "contents";
+    document.getElementById("connexionPrincipale").style.display = "none";
+    document.getElementById("inscriptionPrincipale").style.display = "none";
+    document.getElementById("retourPrincipale").style.display = "";
+
+
+});
+document.getElementById("connexionPrincipale").addEventListener("click", () => {
+    document.getElementById("deuxieme").style.display = "contents";
+    document.getElementById("inscriptionPrincipale").style.display = "none";
+    document.getElementById("connexionPrincipale").style.display = "none";
+    document.getElementById("retourPrincipale").style.display = "";
+
+});
+document.getElementById("retourPrincipale").addEventListener("click", () => {
+    document.getElementById("troisieme").style.display = "none";
+    document.getElementById("connexionPrincipale").style.display = "";
+    document.getElementById("inscriptionPrincipale").style.display = "";
+    document.getElementById("deuxieme").style.display = "none";
     document.getElementById("retourPrincipale").style.display = "none";
 
-    document.getElementById("inscriptionPrincipale").addEventListener("click", () => {
-        document.getElementById("troisieme").style.display = "contents";
-        document.getElementById("connexionPrincipale").style.display = "none";
-        document.getElementById("inscriptionPrincipale").style.display = "none";
-        document.getElementById("retourPrincipale").style.display = "";
-
-
-    });
-    document.getElementById("connexionPrincipale").addEventListener("click", () => {
-        document.getElementById("deuxieme").style.display = "contents";
-        document.getElementById("inscriptionPrincipale").style.display = "none";
-        document.getElementById("connexionPrincipale").style.display = "none";
-        document.getElementById("retourPrincipale").style.display = "";
-
-    });
-    document.getElementById("retourPrincipale").addEventListener("click", () => {
-        document.getElementById("troisieme").style.display = "none";
-        document.getElementById("connexionPrincipale").style.display = "";
-        document.getElementById("inscriptionPrincipale").style.display = "";
-        document.getElementById("deuxieme").style.display = "none";
-        document.getElementById("retourPrincipale").style.display = "none";
-
-    });
-}
->>>>>>> Stashed changes
+});
